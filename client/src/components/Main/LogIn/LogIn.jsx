bda69b01a34fab25c1139f94cc42920d2fbcdb92
ElenaDRFS <elenafullstack@gmail.com>
2023-12-16 21:49:21 +0100

    estilos

--- client/src/components/Main/LogIn/LogIn.jsx
+++ client/src/components/Main/LogIn/LogIn.jsx
@@ -45,17 +45,24 @@ const LogIn = () => {
  
   }
  
+  const handleSignUp = () => {  
+    navigate("/signup");
 
+  }
    
 
-  return <form onSubmit={((e)=> handleClick(e))}> 
-  <h1>Inicio de sesión</h1>
-  <p>Introduce el email y contraseña con el que te registraste</p>
-  <label htmlFor="email"><input type="text" placeholder="Email" name="email" onChange={(e) => setEmail(e.target.value)}/></label> 
-  <label htmlFor="password"><input type="text" placeholder="Contraseña" name="password"/></label> 
-  <button type="submit">Empezar</button>
+  return <section>
+  <form className="login-form"onSubmit={((e)=> handleClick(e))}> 
+      <h1>Inicio de sesión</h1>
+      <p>Introduce el email y contraseña con el que te registraste</p>
+      <label htmlFor="email"><input  className="inputs" type="text" placeholder="Email" name="email"  onChange={(e) => setEmail(e.target.value)} autofocus/></label> 
+      <label htmlFor="password"><input className="inputs" type="text" placeholder="Contraseña" name="password"/></label> 
+      <button className="submit-button"type="submit">Empezar</button>
+ 
  
 </form>;
+ <button className="normal-button" onClick={handleSignUp}>Registrar</button>
+ </section>
 };
 
 export default LogIn;
