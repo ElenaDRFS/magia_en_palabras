af02c76e77f8270e79e831fef9ac6287945a7589
ElenaDRFS <elenafullstack@gmail.com>
2023-12-17 02:27:10 +0100

    sass admin terminado

--- client/src/components/Main/AdminDashboard/AdminDashboard.jsx
+++ client/src/components/Main/AdminDashboard/AdminDashboard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate} from "react-router-dom";
 
+
 const Dashboard = () => {   
   const navigate = useNavigate();
 
@@ -14,12 +15,19 @@ const Dashboard = () => {
 
   }
 
+  const logOut = async () => {
+    navigate("/")
+  };
+
+
   return <section>
     <h1>¿Qué quieres hacer hoy?</h1>
     <button type="button" className="submit-button" onClick={handleCreate}>Crear un nuevo cuento</button>
     
    
     <button type="button" className="submit-button" onClick={handleEdit}>Editar un cuento existente </button>
+  
+    <button className="normal-button" onClick={logOut}>Cerrar sesión</button>
     
     </section>;
 };
