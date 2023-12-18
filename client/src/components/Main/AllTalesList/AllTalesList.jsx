4587f6dc24ad2e6ad8e5657f146a93cf86fc9365
ElenaDRFS <elenafullstack@gmail.com>
2023-12-18 20:14:53 +0100

    styled cards

--- client/src/components/Main/AllTalesList/AllTalesList.jsx
+++ client/src/components/Main/AllTalesList/AllTalesList.jsx
@@ -9,6 +9,9 @@ import AdminCard from "./AdminCard/AdminCard"
 
 const AllTales = () => {
   const [allTales, setAllTales] = useState([]); //todos los cuentos bbdd
+// estados paginación, el primero dirá en que pag se empieza, el segundo los cuentos por pag
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(3); 
 
   useEffect(()=>{
     const getAllTales = async () => {
@@ -24,19 +27,40 @@ const AllTales = () => {
     getAllTales();
   },[]) 
 
+  // mapeamos los cuentos por pag para pintarlos. si lo hacemos con los de la bbdd se pintan todos seguidos, hay que mapear los que queremos que se pinten por pag
   const mapTales = () =>{
-    return allTales.map(obj =>(
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentTales = allTales.slice(indexOfFirstItem, indexOfLastItem);
+
+    return currentTales.map(obj =>(
       <AdminCard key ={uuidv4()} data ={obj}/>
     ))
   }
 
+  // creamos los controles de navegación
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(allTales.length / itemsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+  const renderPageNumbers = pageNumbers.map(number => (
+    <button key={number}>
+      <a
+        href="#"
+        onClick={() => setCurrentPage(number)}
+      >
+        {number}
+      </a>
+    </button>
+  ));
+
   return <div>
     <Link to="/admin"><img src="./volver.png" alt="flecha" /></Link>
+    <section id="page-numbers">
+      {renderPageNumbers}
+    </section>
 
     {mapTales()}
-  
-
-   
     
     </div>
 };
