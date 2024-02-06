e72d060cdfc8696fa1b3146f1eba409a195f69ee
ElenaDRFS <elenafullstack@gmail.com>
2024-02-06 14:21:18 +0100

    header sass done

--- client/src/components/Main/AdminDashboard/AdminDashboard.jsx
+++ client/src/components/Main/AdminDashboard/AdminDashboard.jsx
@@ -1,46 +1,46 @@
 import React from "react";
 
-import { useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-
-const Dashboard = () => {   
+const Dashboard = () => {
   const navigate = useNavigate();
 
-  const handleCreate = () => {  
+  const handleCreate = () => {
     navigate("/createTale");
-
-  }
-
-  const handleEdit = () => {  
-    navigate("/editTale");
-
-  }
-
-  const handleShow = () => {  
-    navigate("/allTales");
-
-  }
-
-
-  const logOut = async () => {
-    navigate("/")
   };
 
-  
+  const handleEdit = () => {
+    navigate("/editTale");
+  };
 
+  const handleShow = () => {
+    navigate("/allTales");
+  };
 
-  return <section>
-    <h1>¿Qué quieres hacer hoy?</h1>
-    <button type="button" className="submit-button" onClick={handleCreate}>Crear un nuevo cuento</button>
-   
-    <button type="button" className="submit-button" onClick={handleEdit}>Editar un cuento existente </button>
-  
-    <button type="button" className="submit-button" onClick={handleShow}>Ver todos los cuentos</button>
+  const logOut = async () => {
+    navigate("/");
+  };
 
-    <button className="normal-button" onClick={logOut}>Cerrar sesión</button>
-    
-    </section>;
+  return (
+    <section>
+      <h1>¿Qué quieres hacer hoy?</h1>
+      <button type="button" className="submit-button" onClick={handleCreate}>
+        Crear un nuevo cuento
+      </button>
+
+      <button type="button" className="submit-button" onClick={handleEdit}>
+        Editar un cuento existente{" "}
+      </button>
+
+      <button type="button" className="submit-button" onClick={handleShow}>
+        Ver todos los cuentos
+      </button>
+
+      <button className="normal-button" onClick={logOut}>
+        Cerrar sesión
+      </button>
+    </section>
+  );
 };
 
 export default Dashboard;
-
--- client/src/components/Main/AllTalesList/AllTalesList.jsx
+++ client/src/components/Main/AllTalesList/AllTalesList.jsx
@@ -1,68 +1,62 @@
 //muestra todos los cuentos y un botón de borrar que hace el axios delete
 
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { v4 as uuidv4 } from "uuid";
 import { Link } from "react-router-dom";
-import AdminCard from "./AdminCard/AdminCard"
-
+import AdminCard from "./AdminCard/AdminCard";
 
 const AllTales = () => {
   const [allTales, setAllTales] = useState([]); //todos los cuentos bbdd
-// estados paginación, el primero dirá en que pag se empieza, el segundo los cuentos por pag
+  // estados paginación, el primero dirá en que pag se empieza, el segundo los cuentos por pag
   const [currentPage, setCurrentPage] = useState(1);
-  const [itemsPerPage] = useState(3); 
+  const [itemsPerPage] = useState(3);
 
-  useEffect(()=>{
+  useEffect(() => {
     const getAllTales = async () => {
-      try{
-        const response = await axios.get("http://localhost:3000/api/tales")
-        
-        setAllTales(response.data);
+      try {
+        const response = await axios.get("http://localhost:3000/api/tales");
 
-      }catch(error){
-        console.log(error)
+        setAllTales(response.data);
+      } catch (error) {
+        console.log(error);
       }
-    }
+    };
     getAllTales();
-  },[]) 
+  }, []);
 
   // mapeamos los cuentos por pag para pintarlos. si lo hacemos con los de la bbdd se pintan todos seguidos, hay que mapear los que queremos que se pinten por pag
-  const mapTales = () =>{
+  const mapTales = () => {
     const indexOfLastItem = currentPage * itemsPerPage;
     const indexOfFirstItem = indexOfLastItem - itemsPerPage;
     const currentTales = allTales.slice(indexOfFirstItem, indexOfLastItem);
 
-    return currentTales.map(obj =>(
-      <AdminCard key ={uuidv4()} data ={obj}/>
-    ))
-  }
+    return currentTales.map((obj) => <AdminCard key={uuidv4()} data={obj} />);
+  };
 
   // creamos los controles de navegación
   const pageNumbers = [];
   for (let i = 1; i <= Math.ceil(allTales.length / itemsPerPage); i++) {
     pageNumbers.push(i);
   }
-  const renderPageNumbers = pageNumbers.map(number => (
+  const renderPageNumbers = pageNumbers.map((number) => (
     <button key={number}>
-      <a
-        href="#"
-        onClick={() => setCurrentPage(number)}
-      >
+      <a href="#" onClick={() => setCurrentPage(number)}>
         {number}
       </a>
     </button>
   ));
 
-  return <div>
-    <Link to="/admin"><img src="./volver.png" alt="flecha" /></Link>
-    <section id="page-numbers">
-      {renderPageNumbers}
-    </section>
+  return (
+    <div>
+      <Link to="/admin">
+        <img src="./src/assets/img/volver.png" alt="flecha" />
+      </Link>
+      <section id="page-numbers">{renderPageNumbers}</section>
 
-    {mapTales()}
-    
+      {mapTales()}
     </div>
+  );
 };
 
 export default AllTales;
