232cac1c3c5b31dbf8478ab7473e184490ca2635
ElenaDRFS <elenafullstack@gmail.com>
2024-02-08 19:38:15 +0100

    audio

--- client/src/components/Main/AllTalesList/AdminCard/AdminCard.jsx
+++ client/src/components/Main/AllTalesList/AdminCard/AdminCard.jsx
@@ -1,9 +1,8 @@
-import React, {useState} from 'react';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import React, { useState } from "react";
 
-function CardDesign({data}) {
-// desplegar detalles. usaremos un estado true/false para el condicional del return. Si show details es true, se enseñan los detalles, sino se esconden, además cambia el texto del botón
+
+function CardDesign({ data }) {
+  // desplegar detalles. usaremos un estado true/false para el condicional del return. Si show details es true, se enseñan los detalles, sino se esconden, además cambia el texto del botón
   const [showDetails, setShowDetails] = useState(false);
 
   const toggleDetails = () => {
@@ -11,21 +10,37 @@ function CardDesign({data}) {
   };
 
   return (
-    <Card className='card-container' style={{ width: '18rem' }}>
-      <Card.Img className='card-img' variant="top" src={data.image} />
-      <Card.Body>
-        <Card.Title className='card-title'>{data.title}</Card.Title>
-     
-        <Button variant="primary"className='card-button' onClick={toggleDetails}> {showDetails ? 'Ocultar detalles' : 'Ver detalles'}</Button>
-        {showDetails && (
-        <article>
-          <h4>Historia:</h4>
-          <p>{data.storie}</p>
-          <audio controls src={data.audio} type="audio/mp3"></audio>
-        </article>
-      )}
-      </Card.Body>
-    </Card>
+   
+      <section className="card-container">
+        <img className="card-img" variant="top" src={data.image} />
+        <section>
+          <h2 className="card-title">{data.title}</h2>
+
+          <button
+            variant="primary"
+            className="card-button"
+            onClick={toggleDetails}
+          >
+            {/* {" "} */}
+            {showDetails ? "Ocultar detalles" : "Ver detalles"}
+          </button>
+          {showDetails && (
+           <section>
+              <p id="storie">{data.storie}</p>
+              <audio controls src={data.audio} type="audio/mp3"></audio>
+
+           </section>
+          ) 
+          // && (<p id="storie">{data.storie}</p>)
+              
+           
+
+            
+          }
+
+        </section>
+      </section>
+    
   );
 }
 
