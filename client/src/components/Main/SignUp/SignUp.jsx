fc23d4934e81e7d3f52fddedd6be68a6d00ee579
ElenaDRFS <elenafullstack@gmail.com>
2024-02-08 13:36:47 +0100

    sass sign up complete

--- client/src/components/Main/SignUp/SignUp.jsx
+++ client/src/components/Main/SignUp/SignUp.jsx
@@ -1,70 +1,82 @@
-
-import React, { useState } from 'react';
-import { auth, db } from '../../../firebase';
+import React, { useState } from "react";
+import { auth, db } from "../../../firebase";
 import { createUserWithEmailAndPassword, signOut } from "firebase/auth";
 import { collection, addDoc } from "firebase/firestore";
-import { useNavigate} from "react-router-dom"; //este hook para redirigir al terminar de registrarnos
+import { useNavigate } from "react-router-dom"; //este hook para redirigir al terminar de registrarnos
 
 import Swal from "sweetalert2";
 
-
 const RegistrationForm = () => {
   //en 2 estados guardamos cada uno de los datos, se setea el estado cuando cambie el input. A medida que se escriba se irá seteando
-  const [email, setEmail] = useState(''); 
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
   const handleRegistration = async (e) => {
-    e.preventDefault();  //evitamos recargo de página
-    
+    e.preventDefault(); //evitamos recargo de página
+
     try {
-    //  creamos nuevo usuario con ese metodo del auth de firestore
-      await createUserWithEmailAndPassword(auth,email, password);
+      //  creamos nuevo usuario con ese metodo del auth de firestore
+      await createUserWithEmailAndPassword(auth, email, password);
       // creamos nuevo usuario en database con los datos que lleguen por el formulario y añadimos el rol y el registered para luego comprobar si ya se está usando ese email
       const docRef = await addDoc(collection(db, "users"), {
         email: email,
         password: password,
-        role: 'client',
-        registered: true
+        role: "client",
+        registered: true,
       });
-      
 
       Swal.fire({
         title: "¡Gracias!",
         text: "Ya has creado tu usuario,serás redirigido al Log in",
-        icon: "success"
+        icon: "success",
       });
-      
-      
-      navigate("/login");
 
+      navigate("/login");
     } catch (error) {
       console.error(error.message);
     }
   };
 
-  const handleBack = () => {  
+  const handleBack = () => {
     navigate("/login");
-
-  }
+  };
 
   return (
     <section>
-    <form onSubmit={handleRegistration}>
-      <h1>Registro</h1>
-      <h3>Introduce un email y una contraseña.</h3>
-      <label htmlFor="email">Email</label>
-      <input className="inputs" type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-     
-      <label htmlFor="password">Constraseña</label>
-      <input className="inputs" type="password" name="password" placeholder='Min. 8 caracteres y 1 número' value={password} onChange={(e) => setPassword(e.target.value)} />
-      
-      <button className="submit-button" type="submit">Registrar</button>
-    </form>
-    <button className="normal-button" onClick={handleBack}>Inicio</button>
+      <form className="login-form" onSubmit={handleRegistration}>
+        <h1>Registro</h1>
+        <h3>Introduce un email y una contraseña.</h3>
+        <label className="sign-label"  htmlFor="email">
+          Email
+          <input
+            className="inputs sign"
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label className="sign-label"  htmlFor="password">Contraseña
+        <input
+          className="inputs sign"
+          type="password"
+          name="password"
+          placeholder="Min. 8 caracteres y 1 número"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        </label>
 
+        <button className="submit-button" type="submit">
+          Registrar
+        </button>
+      </form>
+      <button className="normal-button" onClick={handleBack}>
+        Inicio
+      </button>
     </section>
   );
 };
 
-export default RegistrationForm; 
+export default RegistrationForm;
