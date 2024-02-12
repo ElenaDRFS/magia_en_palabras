42a71b83099f4b0de3dfe2315e5c3fba31bc733e
ElenaDRFS <elenafullstack@gmail.com>
2024-02-12 12:51:36 +0100

    forms

--- client/src/components/Main/SearchPage/Search/Search.jsx
+++ client/src/components/Main/SearchPage/Search/Search.jsx
@@ -1,83 +1,113 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Swal from "sweetalert2";
-import { useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-
-
-
-
-const Search = ({search, press, info}) => {
-  // info son todos los cuentos, lo usaremos para pintar los botones
-
+const Search = ({ search, press, info }) => {
   const navigate = useNavigate();
-  const[input, setInput] = useState("");
+  const [input, setInput] = useState("");
+  const resultsRef = useRef(null); // creamos una referencia para el espacio donde se muestran os cuentos, y hacemos referencia para manda ahí la patalla con el scrollintoview
+
 
   const logOut = async () => {
-    navigate("/")
+    navigate("/");
   };
 
-  const handleClick = (event,value) =>{
+  const handleClick = (event, value) => {
     event.preventDefault();
     press(value);
-   
-  }
+    scrollToResults();
+  };
 
-  const handleSubmit = (event) => {
+  const scrollToResults = () => {
+    resultsRef.current.scrollIntoView({ behavior: "smooth", block: "start" });
+  };
 
-      event.preventDefault();
-      if(!event.target.title.value){
-        Swal.fire("Escribe algún título o palabra que contenga");
-      }else{
-      search(event.target.title.value) //rellenamos el estado de main con lo que se ponga en el input
-      setInput("")  
-      
-  }}
+  const handleSubmit = (event) => {   //tenemos que manejar el error de que no haya coincidencias de título
+    event.preventDefault();
+    if (!event.target.title.value) {
+      Swal.fire("Escribe algún título o palabra que contenga");
+    } else {
+      search(event.target.title.value);
+      setInput("");
+      scrollToResults(); 
+    }
+  };
+
   
 
-  
-// creamos un boton para cada personaje, como en los pokeapi. 
   const createCharacter = () => {
-  
-      const uniqueCharacters = new Set(info.map((item) => item.character));
-      const uniqueCharacterArray = [...uniqueCharacters];
+    const uniqueCharacters = new Set(info.map((item) => item.character));
+    const uniqueCharacterArray = [...uniqueCharacters];
 
-      // mapeamos el array de unicos para crear las options
-      const options = uniqueCharacterArray.map((character, i) => (
-        <button className="character-button"type="click" key={i} value={character} onClick={(event) => handleClick(event, character)}>
-          {character}
-        </button>
-      ));
+    const options = uniqueCharacterArray.map((character, i) => (
+      <button
+        className="character-button"
+        type="click"
+        key={i}
+        value={character}
+        onClick={(event) => handleClick(event, character)}
+      >
+        {character}
+      </button>
+    ));
 
-      return options;
-};
+    return options;
+  };
 
-  return <section>
-
-      <h3>Si quieres salir pulsa</h3>
-      <button id="log-out" className="normal-button" onClick={logOut}>Cerrar sesión</button>
-      <svg id="svg-search" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#392759" fill-opacity="0.7" d="M0,64L120,85.3C240,107,480,149,720,160C960,171,1200,149,1320,138.7L1440,128L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"></path></svg>
-
-
-      <article>
-        <form onSubmit={handleSubmit}>
-          <input className="inputs" type="text"  placeholder="buscador por título" name="title" value={input} onChange={(e) => setInput(e.target.value)}/>
-          <button
-          className="submit-button"type="submit">Buscar</button>
+  return (
+    <section>
+      <section id="welcome-section">
+      
+        <form id="search-form" onSubmit={handleSubmit}>
+          <input
+            id="search-input"
+            type="text"
+            placeholder="buscador por título"
+            name="title"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button id="search-button" type="submit">
+            Buscar
+          </button>
         </form>
-      </article>
+      
 
-      <article>
-        <p>Clica en los botones para buscar por personajes o temáticas</p>
-      </article> 
-      <article>
-        {createCharacter()}
-      </article>
-        
-        
-         
+      <button id="log-out" className="normal-button" onClick={logOut}>
+        Cerrar sesión
+      </button>
+
+      </section>
+      
+      <svg
+        id="svg-search"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1440 320"
+      >
+        <path
+          fill="#392759"
+          fillOpacity="0.7" // Corregir el nombre de la propiedad
+          d="M0,64L120,85.3C240,107,480,149,720,160C960,171,1200,149,1320,138.7L1440,128L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"
+        ></path>
+      </svg>
 
       
-    </section>;
+    <section id="buttons-section">
+    <article >
+        <p id="instructions">Clica en los botones para buscar por personajes o temáticas</p>
+      </article>
+      <article>{createCharacter()}</article>
+
+    </section>
+      
+
+      {/* Referencia para la sección de resultados */}
+
+      <section className="results" ref={resultsRef}>
+
+      </section>
+    </section>
+  );
 };
 
 export default Search;
