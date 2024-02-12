6b62854abe61fbb3ef86d538d2fc29c52e172cbb
ElenaDRFS <elenafullstack@gmail.com>
2024-02-12 13:01:32 +0100

    uncommet production

--- index.js
+++ index.js
@@ -46,11 +46,11 @@ app.use("*", (req, res) => {
 
 
 //* UNIÓN DE BACK Y FRONT PARA EL DESPLIEGUE
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
   
-//   app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
-// }
+  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+}
 
 app.listen(port, () => {
   console.log(`listening on port http://localhost:${port}`);
