b9d3eff8fff90936231391eeaccec05850cc18e2
ElenaDRFS <elenafullstack@gmail.com>
2024-02-13 11:11:06 +0100

    checked

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css';
 import {BrowserRouter} from "react-router-dom";
-
 import Header from "./components/Header";
 import Footer from "./components/Footer"
 import Main from './components/Main/Main';
--- index.js
+++ index.js
@@ -1,44 +1,28 @@
 const express = require("express");
 require("dotenv").config();
-const path = require('path');
+const path = require("path");
 const app = express();
 const port = 3000;
 const morgan = require("./middlewares/morgan");
-const cors = require('cors');
+const cors = require("cors");
 
 require("./config/mongoAtlasConnection"); //nos conectamos a Atlas
 
-// const helmet = require('helmet');
-
 
 const apiRoutes = require("./routes/api.routes");
-const clientRoutes = require('./routes/client.routes');
-
+const clientRoutes = require("./routes/client.routes");
 
 // habilitamos la lectura y recepción de jsons(sino no e reciben los datos del body)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({origin:'*'}))
+app.use(cors({ origin: "*" }));
 
 app.use(morgan(":method :host :status :url :response-time ms :body"));
-// app.use(
-//     //fix de helmet para que se muestren las imagenes de las tarjetas
-//     helmet.contentSecurityPolicy({
-//       useDefaults: true,
-//       directives: {
-//         "img-src": ["'self'", "https: data:"],
-//         "script-src": ["'self'", "https://cdn.jsdelivr.net"],
-//         "frame-src": ["'self'", "https://www.youtube.com"]
-//       },
-//     })
-//   );
-
 
 
 //Rutas
 app.use("/api", apiRoutes); // rutas con peticiones http ADMIN
-app.use("/api",clientRoutes); //rutas con las peticiones http CLIENT
-
+app.use("/api", clientRoutes); //rutas con las peticiones http CLIENT
 
 // Para rutas no existentes
 // app.use("*", (req, res) => {
@@ -47,14 +31,17 @@ app.use("/api",clientRoutes); //rutas con las peticiones http CLIENT
 //   });
 // });
 
-
 //* UNIÓN DE BACK Y FRONT PARA EL DESPLIEGUE
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client'));
+  //*Set static folder
+  app.use(express.static('client/dist'));
   
-  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client','index.html')));
+  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'dist','index.html')));
 }
 
-const PORT = process.env.PORT || 5000;
+//para rutas no existentes
+app.use('*',error404)
 
-app.listen(PORT, () => console.log(`Server started port ${PORT}`));
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+});
