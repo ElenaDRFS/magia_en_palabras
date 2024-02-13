2eb6f46fecf736bbed0fcc77a347336856b85d53
ElenaDRFS <elenafullstack@gmail.com>
2024-02-13 10:08:55 +0100

    uncomment build

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.use("/api",clientRoutes); //rutas con las peticiones http CLIENT
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   
-  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client','index.html')));
 }
 
 const PORT = process.env.PORT || 5000;
