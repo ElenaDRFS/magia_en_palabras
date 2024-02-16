2294ee69747b2ce9ef375470307a41600bcf390a
ElenaDRFS <elenafullstack@gmail.com>
2024-02-16 11:58:22 +0100

    format document

--- client/src/components/Main/SearchPage/SearchPage.jsx
+++ client/src/components/Main/SearchPage/SearchPage.jsx
@@ -1,84 +1,69 @@
 // componente que hará 3 peticiones y renderizará los otros componentes hijo
 
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Search from "./Search/Search"
-import ClientTaleList from "./ClientTaleList/ClientTaleList"
-
+import Search from "./Search/Search";
+import ClientTaleList from "./ClientTaleList/ClientTaleList";
 
 const SearchPage = () => {
-
   // SEARCH SETEA TITLE, CHARACTER
   //CLIENTLIST RECIBE RESULTS Y PINTA TARJETAS
 
-  const [title, setTitle] =  useState(""); // guardamos el título seteado por el buscador, así al cambiar activa el get,además se usa para lanzar la petición. 
-  const [character, setCharacter] =  useState(""); // lo mismo con los personajes
+  const [title, setTitle] = useState(""); // guardamos el título seteado por el buscador, así al cambiar activa el get,además se usa para lanzar la petición.
+  const [character, setCharacter] = useState(""); // lo mismo con los personajes
   const [allTales, setAllTales] = useState([]); //todos los cuentos bbdd
 
-  const [results, setResults] = useState([]) //guardamos resultados de las búsquedas, lo pasaremos por props para pintarlos
+  const [results, setResults] = useState([]); //guardamos resultados de las búsquedas, lo pasaremos por props para pintarlos
 
-
-
-
-  useEffect(()=>{
+  useEffect(() => {
     const getAllTales = async () => {
-      try{
-        const response = await axios.get("/api/tales")
-        
+      try {
+        const response = await axios.get("/api/tales");
+
         setAllTales(response.data);
-
-      }catch(error){
-        console.log(error)
+      } catch (error) {
+        console.log(error);
       }
-    }
+    };
     getAllTales();
-  },[]) 
+  }, []);
 
-  useEffect(()=>{
-    // le añadimos el condicionante para que solo lance la petición cuando haya algún valor en el estado, sin ello, al renderizar el componente lanza la petición y crashea. de esta forma, se esperaráa que haya un valor en ese estado para lanzar el use effect
-    if(title){
+  useEffect(() => {
+    if (title) {
       const getTalesByTitle = async () => {
-     
-        try{
-          const response = await axios.get(`http://localhost:3000/api/titleTales/${title}`)
-          
+        try {
+          const response = await axios.get(`/api/titleTales/${title}`);
+
           setResults(response.data);
-  
-        }catch(error){
-          console.log(error)
+        } catch (error) {
+          console.log(error);
         }
-      }
+      };
       getTalesByTitle();
-
     }
-    
+  }, [title]); //cuando cambie el title, del buscador se vuelve a lanzar la petición get, lo setea search
 
-  },[title]) //cuando cambie el title, del buscador se vuelve a lanzar la petición get
-
-
-  useEffect(()=>{
-    if(character){
+  useEffect(() => {
+    if (character) {
       const getTalesByCharacter = async () => {
-        try{
-          const response = await axios.get(`http://localhost:3000/api/characterTales/${character}`)
-       
+        try {
+          const response = await axios.get(`/api/characterTales/${character}`);
+
           setResults(response.data);
-  
-        }catch(error){
-          console.log(error)
+        } catch (error) {
+          console.log(error);
         }
-      }
+      };
       getTalesByCharacter();
     }
-    
-  },[character]) 
+  }, [character]);
 
-  return <section>
-  
-  <Search search={setTitle} press={setCharacter} info={allTales}/>
-  <ClientTaleList results={results}/>
-
-  </section>;
+  return (
+    <section>
+      <Search search={setTitle} press={setCharacter} info={allTales} />
+      <ClientTaleList results={results} />
+    </section>
+  );
 };
 
 export default SearchPage;
